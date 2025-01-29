4ea2b68ec2a3d148ccd66594bc07e845d8679178
Vishal Kannan <vishalkannan070@gmail.com>
2025-01-29 22:53:37 +0530

    Time: 0 ms (100%), Space: 24.7 MB (75.93%) - LeetHub

--- 1019-squares-of-a-sorted-array/1019-squares-of-a-sorted-array.c
+++ 1019-squares-of-a-sorted-array/1019-squares-of-a-sorted-array.c
@@ -1,15 +1,20 @@
-int cmp(const void* a,const void* b){
-    return (*(int*)a-*(int*)b);
-}
+int cmp(const void* a, const void* b) { return (*(int*)a - *(int*)b); }
 int* sortedSquares(int* nums, int numsSize, int* returnSize) {
-    int ptr=0;
-    int index=0;
-    *returnSize=numsSize;
-    int* result=malloc(numsSize*sizeof(int));
-    while(ptr<numsSize){
-        result[index++]=(nums[ptr]*nums[ptr]);
-        ptr++;
+    int ptr1 = 0;
+    int ptr2 = numsSize - 1;
+    int index = numsSize-1;
+    *returnSize = numsSize;
+    int* result = malloc(numsSize * sizeof(int));
+    while (ptr1 <= ptr2) {
+        if (abs(nums[ptr1]) < abs(nums[ptr2])) {
+            result[index--] = (nums[ptr2] * nums[ptr2]);
+            ptr2--;
+        }
+
+        else {
+            result[index--] = (nums[ptr1] * nums[ptr1]);
+            ptr1++;
+        }
     }
-    qsort(result,numsSize,sizeof(int),cmp);
     return result;
 }
